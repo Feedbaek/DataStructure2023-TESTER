b6de10e431cc57e8b0cc800493aa8da393fa8912
feedbaek <nicek789@naver.com>
2023-05-29 21:46:50 +0900

    Feat: 과제#3 집라인

---
+++ 과제#3/make_test.cpp
@@ -0,0 +1,75 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <utility>
+#include <experimental/filesystem>
+#define EASY 10
+#define HARD 200
+
+using namespace std;
+
+int case_num;
+
+void TEST(int n, int m, int k, int p, int w, FILE *fp, mt19937 &rng) {
+	int edges[202][202] = {};
+	string line;
+	int u, v, c, s, t;
+
+	line = to_string(n) + " ";
+	line += to_string(m) + " ";
+	line += to_string(k) + " ";
+	line += to_string(p) + "\n";
+
+	fputs(line.c_str(), fp);
+
+	for (int j=0; j<m; ++j) {
+		do {
+			u = (rng() % n) + 1;
+			v = (rng() % n) + 1;
+			c = (rng() % w) + 1;
+		} while (u == v || edges[u][v] != 0);
+		edges[u][v] = c;
+		line = to_string(u) + " ";
+		line += to_string(v) + " ";
+		line += to_string(c) + "\n";
+		fputs(line.c_str(), fp);
+	}
+	for (int j=0; j<p; ++j) {
+		s = (rng() % n) + 1;
+		t = (rng() % n) + 1;
+		line = to_string(s) + " ";
+		line += to_string(t) + "\n";
+		fputs(line.c_str(), fp);
+	}
+}
+
+int main(int argc, char** argv) {
+	int n, m, k, p, u, v, c, s, t;
+	string line;
+
+	case_num = atoi(argv[1]);
+	random_device rd;
+	mt19937 rng(rd());
+
+	for (int i=1; i<=case_num; ++i) {
+		string txt = "./myinput/myinput" + to_string(i) + ".txt";
+		FILE *fp = fopen(txt.c_str(), "w");
+
+		if (i <= case_num / 5) {
+			n = 5; m = 7; k = 2; p = 3;
+			TEST(n, m, k, p, 10, fp, rng);
+		}
+		else {
+			n = 200; m = 10000; k = 100; p = 10000;
+			TEST(n, m, k, p, 1000000, fp, rng);
+		}
+		fclose(fp);
+	}
+	return 0;
+}
---
+++ 과제#3/test.sh
@@ -0,0 +1,130 @@
+#!/bin/zsh
+# 파일 명
+NAME="zipline"
+DEFAULT_TEST_NUM=1
+TEST_NUM=100
+COMPILE=g++
+
+./fclean.sh
+
+mkdir myInput/
+mkdir myOutput/
+
+echo "
+=============================================================================================================================
+
+  :::::::::::       ::::::::::       ::::::::   :::::::::::       ::::::::::       :::::::::
+     :+:           :+:             :+:    :+:      :+:           :+:              :+:    :+:
+    +:+           +:+             +:+             +:+           +:+              +:+    +:+
+   +#+           +#++:++#        +#++:++#++      +#+           +#++:++#         +#++:++#:
+  +#+           +#+                    +#+      +#+           +#+              +#+    +#+
+ #+#           #+#             #+#    #+#      #+#           #+#              #+#    #+#
+###           ##########       ########       ###           ##########       ###    ###
+
+      ::::::::::       ::::::::       :::::::::
+     :+:             :+:    :+:      :+:    :+:
+    +:+             +:+    +:+      +:+    +:+
+   :#::+::#        +#+    +:+      +#++:++#:
+  +#+             +#+    +#+      +#+    +#+
+ #+#             #+#    #+#      #+#    #+#
+###              ########       ###    ###
+
+      :::        :::::::::::       :::    :::       ::::::::::        :::        :::::::::::       ::::::::       ::::    :::
+     :+:            :+:           :+:   :+:        :+:               :+:            :+:          :+:    :+:      :+:+:   :+:
+    +:+            +:+           +:+  +:+         +:+               +:+            +:+          +:+    +:+      :+:+:+  +:+
+   +#+            +#+           +#++:++          +#++:++#          +#+            +#+          +#+    +:+      +#+ +:+ +#+
+  +#+            +#+           +#+  +#+         +#+               +#+            +#+          +#+    +#+      +#+  +#+#+#
+ #+#            #+#           #+#   #+#        #+#               #+#            #+#          #+#    #+#      #+#   #+#+#
+########## ###########       ###    ###       ##########        ########## ###########       ########       ###    ####
+
+=============================================================================================================================
+- made by 김민석
+"
+
+echo  "\n#################### compile ####################\n"
+${COMPILE} ${NAME}.cpp -o ${NAME}
+if [ ! -f "$NAME" ]; then
+	echo "[Error] $NAME.cpp 파일이 없습니다!"
+	exit(128)
+fi
+
+${COMPILE} make_test.cpp -o make_test
+${COMPILE} validator.cpp -o validator
+
+echo  "\n#################### default test ####################"
+for num in {1..${DEFAULT_TEST_NUM}}
+do
+	echo "\n--------------------\n"
+	echo "test${num}.txt :"
+	cat ./defaultTest/test${num}.txt
+	echo "\nyour answer :"
+	./${NAME} < ./defaultTest/test${num}.txt
+	echo "real answer :"
+	./validator < ./defaultTest/test${num}.txt
+done
+
+echo "\n#################### make ${TEST_NUM} cases ####################\n"
+./make_test ${TEST_NUM}
+
+echo "\n#################### test ${TEST_NUM} cases ####################\n"
+
+echo "excute your ${NAME}"
+for num in {1..${TEST_NUM}}
+do
+	echo -ne '['
+	for i in {1..${num}}
+	do
+		echo -ne '#'
+	done
+
+	for i in {${num}..100}
+	do
+		echo -ne ' '
+	done
+	echo -ne "(${num}%)]\r"
+
+	./${NAME} < ./myInput/myInput${num}.txt >> ./myOutput/${NAME}_log${num}.txt
+done
+
+echo "\nexcute validator"
+for num in {1..${TEST_NUM}}
+do
+	echo -ne '['
+	for i in {1..${num}}
+	do
+		echo -ne '#'
+	done
+	for i in {${num}..100}
+	do
+		echo -ne ' '
+	done
+	echo -ne "(${num}%)]\r"
+
+	./validator < ./myInput/myInput${num}.txt >> ./myOutput/validator_log${num}.txt
+done
+
+echo "\ncheck your result"
+for num in {1..${TEST_NUM}}
+do
+	echo -ne '['
+	for i in {1..${num}}
+	do
+		echo -ne '#'
+	done
+	for i in {${num}..100}
+	do
+		echo -ne ' '
+	done
+	echo -ne "(${num}%)]\r"
+
+	echo "<<= ${num} =>>" >> ./diff_log.txt
+	diff ./myOutput/${NAME}_log${num}.txt ./myOutput/validator_log${num}.txt >> ./diff_log.txt
+done
+
+#echo "\n\ncheck time complexity"
+#time ./${NAME} < ./bigInput/input1.txt
+#time ./${NAME} < ./bigInput/input2.txt
+#time ./${NAME} < ./bigInput/input3.txt
+
+echo "\n==================== diff_log.txt ===================="
+cat diff_log.txt
